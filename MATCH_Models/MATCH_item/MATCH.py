2c5fdace0ffeaafa9e892005d75571022b6bac7a
reylined <reylined2@gmail.com>
2025-10-01 23:09:24 -0500

    Implemented positive constraint using the Pytorch parametrization module

--- MATCH_Models/MATCH_item/MATCH.py
+++ MATCH_Models/MATCH_item/MATCH.py
@@ -7,7 +7,7 @@ class MATCH(nn.Module):
         super().__init__()
         
         self.item = conv_block(in_channels=n_items*n_cat, out_channels=n_items, dprob=0,
-                                    groups=n_items, kernel_size=1, bias=False)
+                                    groups=n_items, kernel_size=1, bias=False, pos=True)
         
         self.long1 = conv_block(n_items, 16, dprob=0.4, kernel_size=5, padding=3)
         self.mask1 = conv_block(n_items, 8, dprob=0.4, kernel_size=5, padding=3)
@@ -60,14 +60,24 @@ class MATCH(nn.Module):
         x = self.survival(x)
         return x
         
+
+class pos_constraint(nn.Module):
+    def forward(self, x):
+        return F.softplus(x)
+
 class conv_block(nn.Module):
-    def __init__(self, in_channels, out_channels, **kwargs):
+    def __init__(self, in_channels, out_channels, dprob, pos=False, **kwargs):
         super().__init__()
         self.convolution = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, padding_mode='replicate', **kwargs),
             nn.ReLU(),
-            nn.Dropout(0.4),
+            nn.Dropout(dprob),
             nn.BatchNorm1d(out_channels)
             )
+        
+        if pos:
+            nn.utils.parametrize.register_parametrization(self.convolution[0], "weight", pos_constraint())
+        
     def forward(self, x):
             return self.convolution(x)
+
