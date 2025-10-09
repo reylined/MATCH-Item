10c1143b602bbae3c78adb4a67be20fa7afe16e8
reylined <reylined2@gmail.com>
2025-10-08 23:45:37 -0500

    Added configurable parameters

--- MATCH_Models/MATCH_item/MATCH.py
+++ MATCH_Models/MATCH_item/MATCH.py
@@ -3,33 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MATCH(nn.Module):
-    def __init__(self, n_items, n_cat, n_base, out_len):
+    def __init__(self, n_items, n_cat, n_base, out_len,
+                 l1=16, l23=16, l4=16, lmask=8, llin=16):
         super().__init__()
         
         self.item = conv_block(in_channels=n_items*n_cat, out_channels=n_items, dprob=0,
                                     groups=n_items, kernel_size=1, bias=False, pos=True)
         
-        self.long1 = conv_block(n_items, 16, dprob=0.4, kernel_size=5, padding=3)
-        self.mask1 = conv_block(n_items, 8, dprob=0.4, kernel_size=5, padding=3)
+        self.long1 = conv_block(n_items, l1, dprob=0.4, kernel_size=5, padding=3)
+        self.mask1 = conv_block(n_items, lmask, dprob=0.4, kernel_size=5, padding=3)
         
-        self.long2 = conv_block(24, 16, dprob=0.4, kernel_size=5, padding=3)
-        self.mask2 = conv_block(8, 8, dprob=0.4, kernel_size=5, padding=3)
+        self.long2 = conv_block(l1 + lmask, l23, dprob=0.4, kernel_size=5, padding=3)
+        self.mask2 = conv_block(lmask, lmask, dprob=0.4, kernel_size=5, padding=3)
         
-        self.long3 = conv_block(24, 16, dprob=0.4, kernel_size=5, padding=3)
-        self.mask3 = conv_block(8, 8, dprob=0.4, kernel_size=5, padding=3)
+        self.long3 = conv_block(l23 + lmask, l23, dprob=0.4, kernel_size=5, padding=3)
+        self.mask3 = conv_block(lmask, lmask, dprob=0.4, kernel_size=5, padding=3)
         
-        self.long4 = conv_block(24, 16, dprob=0.4, kernel_size=3)
+        self.long4 = conv_block(l23 + lmask, l4, dprob=0.4, kernel_size=3)
         
         self.survival = nn.Sequential(
-            nn.Linear(16 + n_base, 16),
+            nn.Linear(l4 + n_base, llin),
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.BatchNorm1d(16),
-            nn.Linear(16, 16),
+            nn.BatchNorm1d(llin),
+            nn.Linear(llin, llin),
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.BatchNorm1d(16),
-            nn.Linear(16,out_len)
+            nn.BatchNorm1d(llin),
+            nn.Linear(llin,out_len)
         )
             
     
